02a1ae85a183815be47761eb2b6cceeeb8530e00
Clarice Ferreira <clariceoliver432@gmail.com>
2025-10-07 14:14:04 -0300

    configuração da base url e correção da captura de id na criação de etiquetas

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  defaultCommandTimeout: 60000,
+  pageLoadTimeout: 60000,
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl:'https://ddmrp.veaser.com.br',
   },
+  viewportWidth: 1200,
+  viewportHeight: 990,
 });
--- cypress/e2e/etiquetas.cy.js
+++ cypress/e2e/etiquetas.cy.js
@@ -1,20 +1,20 @@
 describe('Fluxo de etiquetas', () => {
 	before(() => {
-		cy.visit('http://localhost:3000/');
+		cy.visit('/');
 		cy.fixture('user.json').then((user) => {
 			cy.login(user.email, user.password);
 		});
 	});
 
 	it('Criar etiqueta', () => {
-		cy.contains('.ant-btn', 'Criar etiqueta', { timeout: 60000 }).click();
+		cy.contains('.ant-btn', 'Criar etiqueta').click();
 		cy.get('.ant-modal-content').should('be.visible');
 		cy.get('#name').type('Etiqueta Teste');
 		cy.get('.ant-form-item-control-input-content > .ant-btn').click();
 		cy.intercept('POST', '/api/Tags').as('criarTag');
 		cy.wait('@criarTag').then((interception) => {
 			expect(interception.response.statusCode).to.eq(201);
-			const tagId = interception.response.body.id;
+			const tagId = interception.response.body.data.id;
 			cy.excluirEtiqueta(tagId);
 		});
 		cy.get('ant-modal-body').should('contain.text', 'Etiqueta Teste');
