330f49eadb492f281dbe7c085b83db22457e78cd
Viviane Ferreira <vferreiradeoliveira595@gmail.com>
2025-10-08 09:48:56 -0300

    CRUD grupos de alocação

--- cypress/e2e/grupos-alocacao.cy.js
+++ cypress/e2e/grupos-alocacao.cy.js
@@ -6,16 +6,47 @@ describe('Fluxo de Grupos de Alocação', () => {
 		});
 	});
 
-	it('Criar Grupo de Alocação', () => {
+	it('CRUD Grupos de Alocação', () => {
 		
 		const nomeGrupo = 'Grupo de Alocação Teste';
-		//cy.intercept('GET', '/api/AllocationGroup/AllocationGroupWithTotals').as('getGrupos')
+		
+		// Create
 		cy.contains('.ant-btn', 'Grupos de alocação priorizada').click();
 		cy.get('.ant-modal-content').should('be.visible');
 		cy.get('#name').type(nomeGrupo);
 		cy.intercept('POST', '/api/AllocationGroup').as('criarGrupo');
 		cy.contains('.ant-form-item-control-input-content > .ant-btn', 'Salvar').click();
 		cy.wait('@criarGrupo')
-		cy.get('ant-modal-body').should('contain.text', nomeGrupo).should('be.visible')
+		cy.contains('td', nomeGrupo ).should('be.visible');
+
+		const nomeGrupoUpdate = 'Grupo de Alocação Atualizado';
+
+		//Update
+		cy.contains('td', nomeGrupo) 
+			.parent('tr') 
+			.within(() => { 
+				cy.contains('button', 'Editar').click() // 
+			})
+		cy.get('#name').clear().type(nomeGrupoUpdate);
+		cy.intercept('PUT', '/api/AllocationGroup/**').as('atualizarGrupo');
+		cy.contains('.ant-form-item-control-input-content > .ant-btn', 'Atualizar').click();
+		cy.wait('@atualizarGrupo')
+		
+		//Delete
+
+		cy.contains('td', nomeGrupo ).should('not.exist')
+		cy.contains('td', nomeGrupoUpdate ).should('be.visible');
+		cy.contains('td', nomeGrupoUpdate) 
+			.parent('tr') 
+			.within(() => { 
+				cy.contains('button', 'Excluir').click() // 
+			})
+		cy.get('.ant-popconfirm').should('be.visible')
+		cy.contains('span', 'OK').click()
+
+
+		cy.get('.ant-modal-content').its('length').should('be.at.least', 1)
+    	cy.contains('td', nomeGrupoUpdate).should('not.exist')
+
 	});
 });
